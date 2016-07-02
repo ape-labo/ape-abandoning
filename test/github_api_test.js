90d0efae86cfdb0e2faec14a5c4b257fd1866474
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-02 21:55:06 +0900

    Version incremented to 1.0.1

---
+++ test/github_api_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for githubApi.
+ * Runs with mocha.
+ */
+'use strict'
+
+const githubApi = require('../lib/helpers/github_api.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('github-api', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Github api', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
